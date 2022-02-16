97ede5c8fd035602a7c3628c4fd05a04b7d63a10
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-02-16 16:27:31 -0300

    dot de cores(bolinhas coloridas)

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,11 +1,60 @@
 //src/pages/Detail
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Detail() {
+import Dot from '../../components/Dot'
+
+export default function Detail({ navigation }) {
+
+  navigation.setOptions({
+    headerTitle: 'Nike Air Max Dia'
+  })
+
   return (
-    <View>
-      <Text>Pagina Detail</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image
+        source={require('../../assets/detail.png')}
+        style={styles.image}
+        resizeMode="cover"
+      />
+
+      <View>
+
+        <View>
+          <Text style={[styles.title, { fontSize: 24 }]}>R$280,90</Text>
+        </View>
+
+        <View opacity={0.4}>
+          <Text style={[styles.title, { fontSize: 30 }]}>Nike Air Max Dia</Text>
+        </View>
+
+        <View style={styles.dotContainer}>
+          <Dot color="#00BFFF" />
+          <Dot color="#FF7F50" />
+          <Dot color="#A9A9A9" />
+          <Dot color="#000000" />
+        </View>
+
+      </View>
+
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#ffffff'
+  },
+  image: {
+    width: '100%',
+  },
+  title: {
+    paddingHorizontal: '2%'
+  },
+  dotContainer: {
+    flexDirection: 'row',
+    marginVertical: '7%'
+  }
+})
